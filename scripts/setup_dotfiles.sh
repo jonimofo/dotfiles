24682f555afb038f5649cb1b870682c6b01a7d67
bgi <bgi@qdv.fr>
2024-11-10 18:10:07 +0700

    add gitignore and update setup_dotfiles script

--- scripts/setup_dotfiles.sh
+++ scripts/setup_dotfiles.sh
@@ -1,34 +1,94 @@
 #!/bin/bash
 
+# Exit if any command fails
+set -e
+
+# Define the dotfiles directory
 DOTFILES_DIR="/home/bgi/Repos/dotfiles/"
 
-# Bash
-ln --symbolic --force "$DOTFILES_DIR"/.bashrc ~/.bashrc
-ln --symbolic --force "$DOTFILES_DIR"/.bash_aliases ~/.bash_aliases
-ln --symbolic --force "$DOTFILES_DIR"/.inputrc ~/.inputrc
-ln --symbolic --force "$DOTFILES_DIR"/.xbindkeysrc ~/.xbindkeysrc
+# Dry run flag (default is false)
+DRY_RUN=true
 
-# Git
-ln --symbolic --force "$DOTFILES_DIR"/.git-completion.bash ~/.git-completion.bash
+# Parse command line arguments
+for arg in "$@"; do
+  case $arg in
+    --dry-run)
+      DRY_RUN=true
+      shift # Remove --dry-run from processing
+      ;;
+  esac
+done
 
-# Vim / Nvim
-ln --symbolic --force "$DOTFILES_DIR"/.vimrc ~/.vimrc
-ln --symbolic --force "$DOTFILES_DIR"/init.vim ~/.config/nvim/init.vim
-ln --symbolic --force "$DOTFILES_DIR"/monokai.vim ~/.vim/colors/monokai
+# Array of file mappings (source, destination)
+declare -a FILES_TO_LINK=(
+    # Bash
+    ".bashrc ~/.bashrc"
+    ".bash_aliases ~/.bash_aliases"
+    ".inputrc ~/.inputrc"
+    ".xbindkeysrc ~/.xbindkeysrc"
+    
+    # Git
+    ".git-completion.bash ~/.git-completion.bash"
+    #TODO git config
+    
+    # Vim / Nvim
+    ".vimrc ~/.vimrc"
+    "init.vim ~/.config/nvim/init.vim"
+    "monokai.vim ~/.vim/colors/monokai"
+    
+    # Tmux
+    ".tmux.conf ~/.tmux.conf"
+    
+    # Terminator
+    "terminator_config.ini ~/.config/terminator/config"
+    
+    # Sublime Text
+    "subl/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
+    "subl/Default\\ \\(Linux\\).sublime-keymap ~/.config/sublime-text-3/Packages/User/Default\\ \\(Linux\\).sublime-keymap"
+    
+    # Espanso
+    "espanso/base.yml ~/.config/espanso/match/base.yml"
+)
 
-# Tmux
-ln --symbolic --force "$DOTFILES_DIR"/.tmux.conf ~/.tmux.conf
+# Create directories if they don't exist
+create_dirs() {
+    echo "Creating necessary directories..."
+    mkdir -p ~/.config/nvim ~/.vim/colors ~/.config/terminator ~/.config/sublime-text-3/Packages/User ~/.config/espanso/match
+}
 
-# Terminator
-ln --symbolic --force "$DOTFILES_DIR"/terminator_config.ini ~/.config/terminator/config
+# Function to create symbolic links or simulate the operation if in dry run mode
+create_symlinks() {
+    for file in "${FILES_TO_LINK[@]}"; do
+        # Split the source and destination
+        src="${file%% *}"
+        dest="${file##* }"
 
-# Sublime Text
-ln --symbolic --force "$DOTFILES_DIR"/subl/Preferences.sublime-settings	 ~/.config/sublime-text-3/Packages/User
-ln --symbolic --force "$DOTFILES_DIR"/subl/Default\ \(Linux\).sublime-keymap	 ~/.config/sublime-text-3/Packages/User
+        if [ "$DRY_RUN" = true ]; then
+            # Dry run: just print the action without executing it
+            echo "[Dry Run] Linking $DOTFILES_DIR/$src -> $dest"
+        else
+            # Actual run: create the symlink
+            echo "Linking $DOTFILES_DIR/$src -> $dest"
+            ln --symbolic --force "$DOTFILES_DIR/$src" "$dest"
+        fi
+    done
+}
 
-# Espanso
-ln --symbolic --force "$DOTFILES_DIR"/espanso/base.yml ~/.config/espanso/match/base.yml
+# Main
+create_dirs
 
-# command -v foo >/dev/null 2>&1 || { echo "I require foo but it's not installed.  Aborting." >&2; exit 1; }
-# TODO vim/nvim
-# TODO setup rest of dotfiles but with checking first if concerned program is installed in user's system
+if [ "$DRY_RUN" = true ]; then
+    echo "Running in dry run mode. No changes will be made."
+else
+    echo "Creating symbolic links..."
+fi
+
+create_symlinks
+
+if [ "$DRY_RUN" = true ]; then
+    echo "Dry run completed. No files were modified."
+else
+    echo "All dotfiles linked successfully!"
+fi
+
+
